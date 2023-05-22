a046ade3a491b46ffa62d906de762b2a87642b23
joashlaw <joashlaw75@gmail.com>
2023-05-22 19:11:24 +0800

    Updated Routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -13,19 +13,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/book',
-      name: 'Books',
-      component: Books,
-    },
-    {
       path: '/',
       name: 'landing',
       component: LandingPage
-    },
-    {
-      path: '/video',
-      name: 'video',
-      component: Video
     },
     {
       path: '/returnuser/:id',
